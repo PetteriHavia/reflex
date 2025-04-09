c62680e841b4b83db907cf49a909f546a7cf3092
Petteri Havia <petsku15@hotmail.fi>
2025-04-09 23:04:23 +0300

    feat: created wrapper component that holds game components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
-import React from "react";
 import Grid from "../components/Grid/Grid";
 import { BsFillLightningFill } from "react-icons/bs";
+import { Link } from "react-router";
+import GameArea from "../components/Layout/GameArea";
 
 const Home = () => {
   return (
     <div>
-      <div className="game-area">
-        <div className="container column">
-          <BsFillLightningFill size={100} color="white" />
-          <h1>Human Benchmark Made With React</h1>
-          <p>Measure your abilities with brain games and cognitive tests.</p>
+      <GameArea>
+        <BsFillLightningFill size={100} color="white" />
+        <h1>Human BeanchMark Made With React</h1>
+        <p>Measure your abilities with brain games and cognitive tests</p>
+        <Link to="/reaction-time">
           <button>Get Started</button>
-        </div>
-      </div>
+        </Link>
+      </GameArea>
       <Grid />
     </div>
   );
