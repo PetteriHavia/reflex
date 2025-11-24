f113d341a8b0f18eb6aa9ae928841b0fa1243e10
petteri <56974048+PetteriHavia@users.noreply.github.com>
2025-11-24 20:49:40 +0200

    chore: add morgan logger middleware

--- server/app.ts
+++ server/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import userRouter from "./controllers/userController";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/errorHandler";
+import logger from "morgan";
 
 const app = express();
+app.use(logger("dev"));
 
 app.use(express.json());
 app.use(cookieParser());
